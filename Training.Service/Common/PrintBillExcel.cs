8a6fd2c25e8246d11087d23931e4ac02ce032511
DESKTOP-2UPET6P\haile <lviethai1997@gmail.com>
2022-04-19 16:09:05 +0700

    edit

--- Training.Service/Common/PrintBillExcel.cs
+++ Training.Service/Common/PrintBillExcel.cs
@@ -8,49 +8,45 @@ namespace Training.Service.Common
     public static class PrintBillExcel
     {
 
-        public static Task<MemoryStream> ExportExcel(OrderDetailView dataview, string filepath)
+        public static async Task<byte[]> ExportExcel(OrderDetailView dataview, string filepath)
         {
-
-            return Task.Run(async () =>
+            Task<byte[]> exportExcelTask = null;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(new FileInfo(filepath)))
             {
-                MemoryStream memStream;
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                using (var package = new ExcelPackage(new FileInfo(filepath)))
-                {
-                    ExcelWorksheet ws = package.Workbook.Worksheets.Add(nameof(dataview));
+                ExcelWorksheet ws = package.Workbook.Worksheets.Add(nameof(dataview));
 
-                    ws.Cells["A2"].Value = "Tên khác hàng: " + dataview.CustommerName;
-                    ws.Cells["A3"].Value = "Địa chỉ: " + dataview.CustommerAddress;
-                    ws.Cells["A4"].Value = "Email: " + dataview.CustommerEmail;
-                    ws.Cells["A5"].Value = "Số điện thoại: " + dataview.CustommerPhone;
-                    ws.Cells["A7"].Value = "Tổng số sản phẩm: " + dataview.TotalQty;
-                    ws.Cells["A8"].Value = "Trị giá đơn hàng: " + dataview.TotalAmount.ToString("N0");
+                ws.Cells["A2"].Value = "Tên khác hàng: " + dataview.CustommerName;
+                ws.Cells["A3"].Value = "Địa chỉ: " + dataview.CustommerAddress;
+                ws.Cells["A4"].Value = "Email: " + dataview.CustommerEmail;
+                ws.Cells["A5"].Value = "Số điện thoại: " + dataview.CustommerPhone;
+                ws.Cells["A7"].Value = "Tổng số sản phẩm: " + dataview.TotalQty;
+                ws.Cells["A8"].Value = "Trị giá đơn hàng: " + dataview.TotalAmount.ToString("N0");
 
-                    ws.Cells["A2:E8"].Style.Font.Bold = true;
-                    ws.Cells["A2:E8"].Style.Font.Size = 18;
+                ws.Cells["A2:E8"].Style.Font.Bold = true;
+                ws.Cells["A2:E8"].Style.Font.Size = 18;
 
-                    ws.Cells["A2:E2"].Merge = true;
-                    ws.Cells["A3:E3"].Merge = true;
-                    ws.Cells["A4:E4"].Merge = true;
-                    ws.Cells["A5:E5"].Merge = true;
-                    ws.Cells["A7:E7"].Merge = true;
-                    ws.Cells["A8:E8"].Merge = true;
+                ws.Cells["A2:E2"].Merge = true;
+                ws.Cells["A3:E3"].Merge = true;
+                ws.Cells["A4:E4"].Merge = true;
+                ws.Cells["A5:E5"].Merge = true;
+                ws.Cells["A7:E7"].Merge = true;
+                ws.Cells["A8:E8"].Merge = true;
 
-                    var tableRange = ws.Cells["A10"].LoadFromCollection(dataview.ListProducts, true, OfficeOpenXml.Table.TableStyles.Dark1);
-                    //ws.Cells[tableRange.Start.Row + 1, 4, tableRange.End.Row, 4].Style.Numberformat.Format = "yyyy-MM-dd";
-                    //ws.Cells[0, 1, dataview.ListProducts.Count + 10, 50].Style.Numberformat.Format = "text";
+                var tableRange = ws.Cells["A10"].LoadFromCollection(dataview.ListProducts,false, OfficeOpenXml.Table.TableStyles.Dark1);
+                //ws.Cells[tableRange.Start.Row + 1, 4, tableRange.End.Row, 4].Style.Numberformat.Format = "yyyy-MM-dd";
+                //ws.Cells[0, 1, dataview.ListProducts.Count + 10, 50].Style.Numberformat.Format = "text";
 
-                    var table = ws.Tables.GetFromRange(tableRange);
-                    table.ShowRowStripes = table.ShowRowStripes;
+                var table = ws.Tables.GetFromRange(tableRange);
+                table.ShowRowStripes = table.ShowRowStripes;
+                ws.Cells.AutoFitColumns();
 
-                    ws.Cells.AutoFitColumns();
-                    
-                    await package.SaveAsync();
 
-                    memStream = new MemoryStream(package.GetAsByteArray());
-                }
-                return memStream;
-            });
+                await package.SaveAsync();
+
+                exportExcelTask = package.GetAsByteArrayAsync();
+            }
+            return await exportExcelTask;
         }
     }
 }
